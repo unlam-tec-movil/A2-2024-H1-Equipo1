157c7e696f34615cc187437b2f282621f9cb4a5a
Juliana Samaniego <jfsamaniego@alumno.unlam.edu.ar>
2024-05-06 20:19:44 -0300

    button view

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/BottomAdd.kt
@@ -0,0 +1,32 @@
+package ar.edu.unlam.mobile.scaffolding.ui.components
+
+import android.media.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.w3c.dom.Text
+
+@Composable
+fun AddButton() {
+    Text(text = "+",
+        modifier = Modifier
+            .padding(10.dp)
+            .drawBehind {
+                drawCircle(
+                    color = Color.Magenta,
+                    radius = 30f
+                )
+            }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMas(){
+    AddButton()
+}
