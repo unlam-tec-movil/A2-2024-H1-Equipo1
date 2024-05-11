210b9ed681d922707342df5b246e634694687b56
mauri <mauricio.caricari@gmail.com>
2024-05-11 10:14:21 -0300

    Agregado de imágenes y modificación del composable

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/PetCard.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/components/PetCard.kt
@@ -1,65 +1,98 @@
 package ar.edu.unlam.mobile.scaffolding.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ar.edu.unlam.mobile.scaffolding.R
+import ar.edu.unlam.mobile.scaffolding.ui.theme.Purple6
 
 @Composable
 fun PetCard(
   petName: String,
   petAge: Int,
+  onClick: () -> Unit,
   modifier: Modifier = Modifier,
-  painter: Painter = painterResource(id = R.drawable.paw_round)
+  painter: Painter = painterResource(id = R.drawable.paw_round),
+  textColor: Color = Color.White,
+  cardColor: Color = Purple6,
+  titleTextSize: TextUnit = 36.sp,
+  subtitleTextSize: TextUnit = 24.sp
 ) {
   Card(
     modifier = modifier
       .padding(4.dp)
       .fillMaxWidth()
       .height(120.dp)
+      .clickable{ onClick() },
+    colors = CardDefaults.cardColors(
+      containerColor = cardColor
+    )
   ) {
     Row(
       modifier = Modifier
         .fillMaxSize()
-        .padding(4.dp),
+        .padding(horizontal = 8.dp, 4.dp)
+        .background(Color.Transparent),
       horizontalArrangement = Arrangement.SpaceEvenly,
       verticalAlignment = Alignment.CenterVertically
     ) {
-      Image(
-        painter = painter,
-        contentDescription = null
-      )
+      Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+          .size(100.dp)
+      ) {
+        Image(
+          painter = painter,
+          contentDescription = null,
+          modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .fillMaxSize()
+            .background(Color.White)
+        )
+      }
       Column(
         modifier = Modifier
           .fillMaxWidth()
-          .padding(8.dp),
+          .padding(8.dp)
+          .background(Color.Transparent),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.SpaceEvenly
       ) {
         CustomText(
           text = petName,
-          fontSize = 32.sp,
-          maxLines = 1
+          fontSize = titleTextSize,
+          maxLines = 1,
+          color = textColor
         )
         CustomText(
           text = stringResource(R.string.pet_age, petAge),
-          fontSize = 24.sp,
-          maxLines = 1
+          fontSize = subtitleTextSize,
+          maxLines = 1,
+          color = textColor
         )
       }
     }
@@ -73,6 +106,8 @@ fun PetCard(
 private fun PetCardPreview() {
   PetCard(
     petName = "Mauri",
-    petAge = 3
+    petAge = 3,
+    painter = painterResource(R.drawable.dog_silhouette_01),
+    onClick = {}
   )
 }
